94c1d5d4e9e5c255ea2b74bf75bad360e49cee63
FariqJavier <fariqjavier@gmail.com>
2025-08-24 20:18:46 +0700

    update welcome section content

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,8 +4,8 @@ const Welcome: React.FC = () => {
   return (
     <section className="bg-gray-600 text-white py-16">
       <div className="max-w-screen-lg mx-auto text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio!</h1>
-        <h3 className="text-xl font-medium">Explore my projects and experience in AI and Robotics.</h3>
+        <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+        <h3 className="text-xl font-medium">Explore my projects and skills in AI, ML, Robotics, and many more!</h3>
         <div className="mt-8">
           <button className="bg-transparent border-2 border-white text-white px-6 py-2 rounded-lg hover:bg-white hover:text-gray-900">
             Contact Me
